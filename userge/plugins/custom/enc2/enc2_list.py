c5f265f6cff0a811446f76281ed374ebe44eda0b
tm5Bar4bhcpVsbsbsniG0WoGiMCF62g2g <93268324+tm5Bar4bhcpVsbsbsniG0WoGiMCF62g2g@users.noreply.github.com>
2021-11-03 00:19:03 +0530

    Update enc2_list.py

--- userge/plugins/custom/enc2/enc2_list.py
+++ userge/plugins/custom/enc2/enc2_list.py
@@ -9,5 +9,10 @@ cmd = "/"
 @app.on_message(filters.command("enc2_list", cmd))
 async def encode_start(app, message):
     await message.reply_text(
-        f"**List of cmd for diff bot:-**\n\n/enc2 - @Encoding_Stuffs_Bot\n/enc2_1 - @WZI_Encoder_Bot"
+        f"""**List of cmd for diff bot:-**
+
+/enc2 - @Encoding_Stuffs_Bot
+/enc2_1 - @WZI_Encoder_Bot
+/enc2_2 - @bACkUpbObot
+/enc2_3 - @bACKUPWZIBOT"""
     )
